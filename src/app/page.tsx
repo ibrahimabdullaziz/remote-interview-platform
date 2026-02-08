660e848f177040ef8b4cec67f6522e74a9ef6004
Ibrahim Abdulaziz <ibrahimabdullaziz55@gmail.com>
2026-02-08 18:47:28 +0200

    feat: add convex backend for real-time data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-import { LogIn, LogOut, User } from "lucide-react";
 
 export default function Home() {
-  return (
-    <div>
-      <SignedOut>
-        <SignInButton>
-          <Button>Sign In</Button>
-        </SignInButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </div>
-  );
+  return <div>home page</div>;
 }
